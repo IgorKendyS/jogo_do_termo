72bc84fa0837d7d44838cb3dd7056668ccd92251
IgorKendyS <igor-kendy@hotmail.com>
2025-09-02 08:58:03 -0300

    Termo Listar Palavras Salvas

---
+++ TermoLib/Termo.cs
@@ -0,0 +1,13 @@
+﻿namespace TermoLib
+{
+    public class Termo
+    {
+        public List<String> palavras;
+
+        public void CarregaPalavras(string fileName)
+        {
+            palavras = File.ReadAllLines(fileName).ToList();
+        }
+
+    }
+}
---
+++ TermoTest/TestTermo.cs
@@ -0,0 +1,16 @@
+﻿using TermoLib;
+
+namespace TermoTest
+{
+    [TestClass]
+    public sealed class TestTermo
+    {
+        [TestMethod]
+        public void TestReadFile()
+        {
+            Termo termo = new Termo();
+            termo.CarregaPalavras("palavras.txt");
+            Console.WriteLine(String.Join("\n",termo.palavras));
+        }
+    }
+}
