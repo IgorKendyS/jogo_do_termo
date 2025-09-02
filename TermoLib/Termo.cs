361f8f62b43d0c267eb4c1bcade92de34487a28c
IgorKendyS <igor-kendy@hotmail.com>
2025-09-02 11:08:44 -0300

    Palavra sorteada Cor em letras implementado Sem interface

--- TermoLib/Termo.cs
+++ TermoLib/Termo.cs
@@ -1,13 +1,74 @@
 ﻿namespace TermoLib
 {
+    public class Letra
+    {
+        public Letra(char caracter, char cor)
+        {
+            Caracter = caracter;
+            Cor = cor;
+        }
+        public char Caracter;
+        public char Cor;
+    }
     public class Termo
     {
         public List<String> palavras;
+        public string palavraSorteada;
+        public List<List<Letra>> tabuleiro;
+        public Dictionary<char, char> teclado;
+        int palavraAtual;
+
+        public Termo()
+        {
+            CarregaPalavras("palavras.txt");
+            SorteiaPalavra();
+            palavraAtual = 1;
+            tabuleiro = new List<List<Letra>>();
+            teclado = new Dictionary<char, char>();
+            for (int i = 65; i <= 90; i++)
+            {
+                // V - posicao correta | A - Na Palavra | P - Nao esta na palavra
+                teclado.Add((char)i, 'C');
+            }
+        }
 
         public void CarregaPalavras(string fileName)
         {
             palavras = File.ReadAllLines(fileName).ToList();
         }
+        public void SorteiaPalavra()
+        {
+            Random rand = new Random();
+            var index = rand.Next(0, palavras.Count() - 1);
+            palavraSorteada = palavras[index];
+        }
 
+        public void ChecaPalavra(string palavra)
+        {
+            if (palavra.Length != 5)
+            {
+                throw new Exception("A palavra deve ter 5 letras");
+            }
+            var palavraTabuleiro = new List<Letra>();
+            char cor;
+            for (int i = 0; i < palavra.Length; i++)
+            {
+                if (palavra[i] == palavraSorteada[i]){
+                    cor = 'V';
+                }
+                else if (palavraSorteada.Contains(palavra[i]))
+                {
+                    cor = 'A';
+                }
+                else
+                {
+                    cor = 'P';
+                }
+                palavraTabuleiro.Add(new Letra(palavra[i], cor));
+            }
+            tabuleiro.Add(palavraTabuleiro);
+            palavraAtual++;
+        }
+        
     }
 }
--- TermoTest/TestTermo.cs
+++ TermoTest/TestTermo.cs
@@ -12,5 +12,33 @@ namespace TermoTest
             termo.CarregaPalavras("palavras.txt");
             Console.WriteLine(String.Join("\n",termo.palavras));
         }
+
+        [TestMethod]
+        public void testJogo()
+        {
+            Termo termo = new Termo();
+            imprimirJogo(termo);
+            termo.ChecaPalavra("APOIO");
+            imprimirJogo(termo);
+        }
+
+        public void imprimirJogo(Termo termo)
+        {
+            Console.WriteLine("PALAVRA SORTEADA: " + termo.palavraSorteada +"\n");
+            Console.WriteLine("T A B U L E I R O");
+            foreach (var palavra in termo.tabuleiro){
+                foreach(var letra in palavra)
+                {
+                    Console.Write(letra.Caracter + ": " + letra.Cor + " | ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("T E C L A D O");
+            foreach (var tecla in termo.teclado)
+            {
+                Console.Write(tecla.Key + ": " + tecla.Value + " | ");
+            }
+            Console.WriteLine("\n\n");
+        }
     }
 }
